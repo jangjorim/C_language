529b023e6813dfaf4b1e0470ba10a3095c33f9f1
jangjorim <nims8006@gmail.com>
2021-07-27 19:47:47 +0900

    구름 level stack 문제를 풀었다.  그러나 테스트 케이스는 통과가 되지만 이외에 추가적인 테스트를 하면 오류가 생긴다.

--- 2021_Personal_Study/Goorm_Level/Algorithm/Stack_Queue/Stack/Stack.c
+++ 2021_Personal_Study/Goorm_Level/Algorithm/Stack_Queue/Stack/Stack.c
@@ -4,67 +4,80 @@
 typedef struct _stack {
 	int stack[10];
 	int topIndex;
+	int bottomIndex;
 } Stack;
-
-int ArrayToInteger(char string[]) {			// 문자열 혹은 문자로 들어온 입력 함수를 정수로 바꾸는 함수
-	int count = 0, num = 0;
-
-	while (string[count] != 0 && string[count] != "\n") {
-		num = num * 10 + (string[count] - '0');
-		count++;
-	}
-	return num;
-}
 
 void SInit(Stack* pstack) {			// 초기에 스택을 초기화 하는 함수;
 	pstack->topIndex = -1;
-}
-
-int SIsEmpty(Stack* pstack) {
-	if (pstack->topIndex == -1)
-		return 1;
-	else
-		return 0;
+	pstack->bottomIndex = 0;
 }
 
 void SPush(Stack* pstack, int Data) {
 	if ((pstack->topIndex += 1) != 10)
 		pstack->stack[pstack->topIndex] = Data;
 	else
-		printf("Stack overflow");
+		printf("overflow\n");
+}
+
+int SIsEmpty(Stack* pstack) {
+	if ((pstack->bottomIndex - 1) != pstack->topIndex)
+		return 1;
+	else
+		return 0;
+}
+
+int SPop(Stack* pstack) {
+	if (SIsEmpty(pstack))
+		return pstack->stack[pstack->bottomIndex++];
+	else
+		printf("underflow\n");
 }
 
 
 int main() {
 	Stack stack;
 
-	int count, NUM, Data, i = 1;
-	char InputData[4], N;
-	
-	//몇 번 입력을 받을 것인지
-	count = ArrayToInteger(gets(InputData));
-	
-	while (count != 0) {
-		NUM = ArrayToInteger(gets(InputData));
-		Data = ArrayToInteger(gets(InputData));
-		if (NUM == 0) {
-			SPush(&stack, Data);
-		}
-		else if (NUM == 1) {
+	SInit(&stack);
 
+	int count, i, Data, IntData[10], time = 0;
+	char Input[4];
+
+	//몇 번 입력을 받을 것인지
+
+	scanf(" %d ", &count);
+
+	for (i = 0; (i + time) < count * 2; i++) {
+
+		scanf("%d ", &Data);
+		if (Data == 1 || Data == 0) {
+
+			if (Data == 1) {
+				IntData[i] = Data;
+			}
+			else {
+				scanf("%d ", &Data);
+				IntData[i] = Data;
+			}
+			time++;
+			continue;
 		}
 		else
-			exit(-1);
+			break;
+
 	}
-	
-		
 
-	
+	for (i = 0; i < time; i++) {
+		Data = IntData[i];
+		if (Data == 1) {
+			SPop(&stack);
+		}
+		else {
+			SPush(&stack, Data);
+		}
+	}
 
+	while (SIsEmpty(&stack))
+		printf("%d ", SPop(&stack));
 
-	
-
-
-
-
+	return 0;
 }
--- 2021_Personal_Study/Goorm_Level/Algorithm/Stack_Queue/Stack/Test.c
+++ 2021_Personal_Study/Goorm_Level/Algorithm/Stack_Queue/Stack/Test.c
@@ -1,38 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct _stack {
-	int stack[10];
-	int topIndex;
-} Stack;
-
-int ArrayToInteger(char string[]) {			// 문자열 혹은 문자로 들어온 입력 함수를 정수로 바꾸는 함수
-	int count = 0, num = 0;
-
-	while (string[count] != 0 && string[count] != "\n") {
-		num = num * 10 + (string[count] - '0');
-		count++;
-	}
-	return num;
-}
-
-
-
-
-int main() {
-	Stack stack;
-
-	int count, NUM, Data, i = 1;
-	char InputData[4], N;
-
-	//몇 번 입력을 받을 것인지
-	count = ArrayToInteger(gets(InputData));
-
-	while (count != 0) {
-		NUM = ArrayToInteger(gets(InputData));
-		printf("%d", NUM);
-		Data = ArrayToInteger(gets(InputData));
-		printf("%d", Data);
-		
-	}
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//typedef struct _stack {
+//	int stack[10];
+//	int topIndex;
+//} Stack;
+//
+//int ArrayToInteger(char string[]) {			// 문자열 혹은 문자로 들어온 입력 함수를 정수로 바꾸는 함수
+//	int count = 0, num = 0;
+//
+//	while (string[count] != 0 && string[count] != "\n") {
+//		num = num * 10 + (string[count] - '0');
+//		count++;
+//	}
+//	return num;
+//}
+//
+//
+//
+//
+//int main() {
+//	Stack stack;
+//
+//	int count, NUM, Data, i = 1;
+//	char InputData[4], N;
+//
+//	//몇 번 입력을 받을 것인지
+//	count = ArrayToInteger(gets(InputData));
+//
+//	while (count != 0) {
+//		NUM = ArrayToInteger(gets(InputData));
+//		
+//		
+//	}
+//}
